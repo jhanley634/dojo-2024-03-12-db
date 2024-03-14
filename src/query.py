45c3bb7905a06ecb8abce55d217080ea42d6c1b4
John Hanley <jhanley708-git@s6.sector6.net>
2024-03-14 09:50:15 -0700

    Add QueryPlan class

--- src/query.py
+++ src/query.py
@@ -4,7 +4,8 @@
 from typing import Any
 
 import sqlalchemy as sa
-from sqlalchemy import Select, TextClause, text
+from sqlalchemy import Row, Select, TextClause, text
+from sqlalchemy.orm import Session
 
 from src.db import get_engine, get_session, get_tables
 
@@ -21,12 +22,26 @@ def example_join_query() -> None:
             .where(movie_actor.c.actor_id.is_("nm0000001"))
             .order_by(movie_actor.c.movie_id)
         )
-        print("\n".join(map(str, sess.execute(explain(q)))))
+        print(QueryPlan(q, sess), "\n\n")
         for row in sess.execute(q):
             print(row._asdict())
 
 
-def explain(q: Select[Any]) -> TextClause:
+class QueryPlan:
+    def __init__(self, q: Select[Any], sess: Session) -> None:
+        self.compiled_query = sess.execute(_get_explanation(q))
+
+    def __str__(self) -> str:
+        def fmt_row(row: Row[Any]) -> str:
+            sqlite_plan_fields = ["id", "parent", "notused", "detail"]
+            d = row._asdict()
+            assert sqlite_plan_fields == list(d.keys()), d
+            return f"{d['id']:3d} {d['parent']:2d}:   {d['detail']}"
+
+        return "\n".join(map(fmt_row, self.compiled_query))
+
+
+def _get_explanation(q: Select[Any]) -> TextClause:
     """Obtains a query plan."""
     compiled = str(
         q.compile(
